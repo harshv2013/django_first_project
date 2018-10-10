68448f91e89fd324edcc62a0f652d4fe03073330
harsh2013 <harsh2013@gmail.com>
2018-10-10 18:10:39 +0530

    Pre Final Update !

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]

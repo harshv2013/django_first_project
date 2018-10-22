fe061f42556230d7466c5f635f0607493194040f
harsh2013 <harsh2013@gmail.com>
2018-10-22 12:41:44 +0530

    setting corrected2.

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-vyd-xct$%)9hx0c^px(1pna(1(&f@x32+e=)m^fsbj)(vce0r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
